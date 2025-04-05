8cc5382556b72b839b5cbfd725d982698e43bb93
devanshu <devborkar211@gmail.com>
2025-04-06 02:43:49 +0530

    Initial commit

---
+++ src/main/java/com/example/Word/Search/System/WordSearchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Word.Search.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordSearchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordSearchSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Word/Search/System/WordSearchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Word.Search.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordSearchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
